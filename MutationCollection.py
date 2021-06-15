ad7db04ed241eb672d25eb5c0a387eb63b01ad68
noah842 <85526614+noah842@users.noreply.github.com>
2021-06-15 11:33:24 -0400

    Refined mutation_collection function

--- MutationCollection.py
+++ MutationCollection.py
@@ -32,15 +32,27 @@ def list_maker(k):
 # Mutations Distance k from seed
 def mutation_collection(cluster, array, k):
     mutation_list = list()
+    lmaker = list_maker(k)
     for i in range(6 * (5 ** (k - 1))):
         for n in range(k):
-            a = int(str(list_maker(k)[i])[n])
+            a = int(str(lmaker[i])[n])
             if n == 0:
                 (x, y) = mutation(cluster, array, a)
             else:
                 (x, y) = mutation(x, y, a)
         mutation_list.append(x)
     return mutation_list
+
+
+# A more elegant (and hopefully more memory efficient) version of the above
+def refined_mutation_collection(cluster, array, k):
+    collection = list()
+    m = mutation_collection(cluster, array, k)
+    for i in range(6 * (5 ** (k - 1))):
+        for j in range(9):
+            if m[i][j] not in collection:
+                collection.append(m[i][j])
+    return collection
 
 
 # 3D Scatter Plot
@@ -58,16 +70,18 @@ def plot(cluster, array, k):
     x = list()
     y = list()
     z = list()
-    m = mutation_collection(cluster, array, k)
-    for i in range(6 * (5 ** (k - 1))):
-        for j in range(9):
-            x.append(m[i][j][0])
-            y.append(m[i][j][1])
-            z.append(m[i][j][2])
+    m = refined_mutation_collection(cluster, array, k)
+    for i in range(len(refined_mutation_collection(cluster, array, k))):
+        x.append(m[i][0])
+        y.append(m[i][1])
+        z.append(m[i][2])
     return [x, y, z]
 
 
-# Make Plot
-p = plot(C, A, 4)
+# Make Plot (some examples
+r = refined_mutation_collection(C, A, 6)
+print(r)
+print(len(r))
+p = plot(C, A, 6)
 ax_1.scatter(p[0], p[1], p[2])
 pyplot.show()

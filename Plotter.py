6e4321b417f85242b06da9e3c47e0e8638bcfe76
Ethan-Kirsch <85592366+Ethan-Kirsch@users.noreply.github.com>
2021-06-14 21:17:04 -0400

    Create Plotter.py

---
+++ Plotter.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib as plt
+
+from MutationFunction import C
+
+#Current filler is using weight cluster W, which I imagine is a list compiled from weight vectors [d, d_1, d_2], yada yada, which I will adapt once we determine how we are storing weight vectors from mutations.
+from FutureFunction import W
+    for i in range(1, len(W)):
+        d = [W[i][0]]
+        d_1 = [W[i][1]]
+        d_2 = [W[i][2]]
+
+#May add query what type of scale we want to graph our weight vectors on
+#scale = input("Plot 'linear' or 'logarithmic':")
+
+#Linear version
+#if scale = linear:
+    plt.plot(d, d_1, d_2)
+    plt.axis(0, max(d), 0, max(d_1), 0, max(d_2))

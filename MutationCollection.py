9af5dad34236efe32f569f0f1b5790d15245a55f
noah842 <85526614+noah842@users.noreply.github.com>
2021-06-15 09:26:28 -0400

    Initial version of function to plot collection of cluster variables. I think the number of them that are plotted is currently not right

--- MutationCollection.py
+++ MutationCollection.py
@@ -1,5 +1,9 @@
 # Preamble
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 from MutationFunction import mutation
+from MutationFunction import A
+from MutationFunction import C
 
 
 # List of good mutations distance k from the seed
@@ -7,15 +11,15 @@ from MutationFunction import mutation
 
 def list_maker(k):
     mutation_list = list()
-    r = list(range(1, 7))
+    r = range(1, 7)
     for i in range(1, k + 1):
         if i == 1:
-            for l in r:
-                m_i = l
+            for q in r:
+                m_i = q
                 mutation_list.append(m_i)
         else:
-            for l in r:
-                m_i = l
+            for q in r:
+                m_i = q
                 for k in range(6 * 5 ** (i - 2)):
                     if m_i != int(str(mutation_list[k])[i - 2]):
                         a = int(str(mutation_list[k]) + str(m_i))
@@ -28,12 +32,42 @@ def list_maker(k):
 # Mutations Distance k from seed
 def mutation_collection(cluster, array, k):
     mutation_list = list()
-    for l in range(6 * (5 ** (k - 1))):
+    for i in range(6 * (5 ** (k - 1))):
         for n in range(k):
-            a = int(str(list_maker(k)[l])[n])
+            a = int(str(list_maker(k)[i])[n])
             if n == 0:
                 (x, y) = mutation(cluster, array, a)
             else:
                 (x, y) = mutation(x, y, a)
         mutation_list.append(x)
     return mutation_list
+
+
+# 3D Scatter Plot
+fig_1 = pyplot.figure()
+ax_1 = Axes3D(fig_1)
+
+# Axis Labels
+ax_1.set_xlabel('X-axis')
+ax_1.set_ylabel('Y-axis')
+ax_1.set_zlabel('Z-axis')
+
+
+# Plotting Weight Vectors
+def plot(cluster, array, k):
+    x = list()
+    y = list()
+    z = list()
+    m = mutation_collection(cluster, array, k)
+    for i in range(6 * (5 ** (k - 1))):
+        for j in range(9):
+            x.append(m[i][j][0])
+            y.append(m[i][j][1])
+            z.append(m[i][j][2])
+    return [x, y, z]
+
+
+# Make Plot
+p = plot(C, A, 4)
+ax_1.scatter(p[0], p[1], p[2])
+pyplot.show()

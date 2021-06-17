a0bdb958ba40a9806a7823d116a0262e2aad0e87
noah842 <85526614+noah842@users.noreply.github.com>
2021-06-17 17:10:34 -0400

    Matrix C for conjugation

--- Task2.py
+++ Task2.py
@@ -1,13 +1,25 @@
 import numpy as np
 
+
 # Random diagonal matrix with distinct eigenvalues
-def matrix(size):
+def matrix_a(size):
     a = np.zeros((size, size), dtype=int)
     b = np.random.permutation(100)[:size]
-    print(b)
     for i in range(size):
         a[i][i] = b[i]
     return a
 
 
-print(matrix(3))
+# Matrix C for conjugation
+def matrix_c(size):
+    c = 2 * np.random.rand(size, size) - 1
+    if np.linalg.det(c) != 0:
+        return c
+
+
+k = 3
+A = matrix_a(k)
+C = matrix_c(k)
+print(A)
+print(C)
+

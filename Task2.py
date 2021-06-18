5648f9e97d5e00978af9c96ddee5473b6682a43d
noah842 <85526614+noah842@users.noreply.github.com>
2021-06-18 12:51:49 -0400

    Test for TNN. Need to put parts together now to generate TNN matrices and figure out how to store them

--- Task2.py
+++ Task2.py
@@ -17,9 +17,34 @@ def matrix_c(size):
         return c
 
 
+# Evidently CAC_ is totally non-negative iff all n^2 initial minors are non-negative (minors of consecutive rows/columns
+# that border the top left or right of matrix), so we'll just check these.
+
+
+def tnn_test(matrix):
+    c = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            m = min(i, j)
+            rows = list()
+            columns = list()
+            for n in range(m + 1):
+                rows.insert(0, (i - n))
+                columns.insert(0, (j - n))
+            a = matrix[np.ix_(rows, columns)]
+            if np.linalg.det(a) >= 0:
+                c = c + 1
+    if c == (len(matrix) + 1) ** 2:
+        return 1
+    else:
+        return 0
+# Note: returns 1 if tnn, else returns 0
+
+
 k = 3
 A = matrix_a(k)
 C = matrix_c(k)
-print(A)
-print(C)
-
+C_ = np.linalg.inv(C)
+CAC_ = np.matmul(C, A, C_)
+print(CAC_)
+print(tnn_test(CAC_))

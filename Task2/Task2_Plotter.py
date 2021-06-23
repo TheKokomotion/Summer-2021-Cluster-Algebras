873e758115b17ed0ee9772c616ea073c6a2184ef
noah842 <85526614+noah842@users.noreply.github.com>
2021-06-23 14:30:50 -0400

    More vectors in D(A)

--- Task2/Task2_Plotter.py
+++ Task2/Task2_Plotter.py
@@ -8,6 +8,9 @@ from D_A_Storage import z_2
 from D_A_Storage import x_3
 from D_A_Storage import y_3
 from D_A_Storage import z_3
+from D_A_Storage import x_4
+from D_A_Storage import y_4
+from D_A_Storage import z_4
 
 
 # 3D Scatter Plot
@@ -20,6 +23,7 @@ ax_1.set_ylabel('Y-axis')
 ax_1.set_zlabel('Z-axis')
 
 # Make Plot
-ax_1.scatter(x_2, y_2, z_2, color='Blue')
+ax_1.scatter(x_2, y_2, z_2, color='Red')
 ax_1.scatter(x_3, y_3, z_3, color='Red')
+ax_1.scatter(x_4, y_4, z_4, color='Red')
 pyplot.show()
